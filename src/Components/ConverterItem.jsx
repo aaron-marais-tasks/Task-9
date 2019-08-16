c6f535c528c496cbf89231e8e717392457a4080f
Aaron Lee Marais <aaronleem@gmail.com>
2019-08-16 16:43:42 +0200

    Increase my comment XP

--- src/Components/ConverterItem.jsx
+++ src/Components/ConverterItem.jsx
@@ -14,26 +14,36 @@ export const ItemConsumer = ItemContext.Consumer
 
 // Item component
 export default class Item extends React.Component {
+	// Our component context type
 	static contextType = ItemContext
 
+	// Calculate amount based on context
 	getTotalAmount = () => {
+		// First try to convert to float (int strips after dot)
 		let currentInAud = parseFloat(this.context.amount)
 
+		// If is not a number, try and convert to integer
 		if(Number.isNaN(currentInAud))
 			currentInAud = parseInt(this.context.amount)
 
+		// If still not a number, set to zero
 		if(Number.isNaN(currentInAud))
 			currentInAud = 0
 
+		// Calculate and return converted amount
 		return currentInAud * this.props.factor
 	}
 
 	render() {
 		return (
+			// Div to hold item
 			<div className="item">
+				{/* Item title */}
 				<span className="title">
 					{this.props.title}
 				</span>
+
+				{/* Converted amount */}
 				<span className="amount">
 					{this.getTotalAmount().toFixed(2)}
 				</span>
